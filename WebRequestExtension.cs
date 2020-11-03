959e6306ab887bba8747500d3893f20c018d95f0
ivanmeler <i_ivan@windowslive.com>
2020-11-03 15:49:23 +0100

    Fix firmware requests broken by samsungs api changes

--- WebRequestExtension.cs
+++ WebRequestExtension.cs
@@ -4,6 +4,7 @@
 // MVID: 14A8B9D4-ACD6-4CE0-9F53-A466F0519E6A
 // Assembly location: C:\Users\Ivan\Desktop\LG Flash Tool 2014\SamFirm\SamFirm.exe
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -18,7 +19,8 @@ namespace SamFirm
       {
         WebResponse response = wr.GetResponse();
         if (((IEnumerable<string>) response.Headers.AllKeys).Contains<string>("Set-Cookie"))
-          Web.JSessionID = response.Headers["Set-Cookie"].Replace("JSESSIONID=", "").Split(';')[0];
+          //Web.JSessionID = response.Headers["Set-Cookie"].Replace("JSESSIONID=", "").Split(';')[0];
+          Web.JSessionID = WebRequestExtension.seal(response.Headers[HttpResponseHeader.SetCookie], "JSESSIONID");
         if (((IEnumerable<string>) response.Headers.AllKeys).Contains<string>("NONCE"))
           Web.Nonce = response.Headers["NONCE"];
         return response;
@@ -30,6 +32,17 @@ namespace SamFirm
           Web.SetReconnect();
         return ex.Response;
       }
+    }
+    private static string seal(string tuljan, string _param1)
+    {
+      string[] strArray = tuljan.Split(new char[2]{ ';', ',' }, StringSplitOptions.RemoveEmptyEntries);
+      string empty = string.Empty;
+      foreach (string str in strArray)
+      {
+        if (str.Contains(_param1))
+          empty = str.Split(new string[1]{ "=" }, StringSplitOptions.RemoveEmptyEntries)[1];
+      }
+      return empty;
     }
   }
 }
