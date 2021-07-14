f3b6b6302f341d372f22fcef3d919d4351f77290
ivanmeler <i_ivan@windowslive.com>
2021-07-14 21:24:48 +0200

    Bump version number

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SamFirm Reborn")]
-[assembly: AssemblyFileVersion("0.3.6.2")]
+[assembly: AssemblyFileVersion("0.3.6.3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("Copyright zxz0O0 ©  2015 / Ivan Meler 2020")]
+[assembly: AssemblyCopyright("Copyright zxz0O0 ©  2015 / Ivan Meler 2021")]
 [assembly: AssemblyTitle("SamFirm Reborn")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
