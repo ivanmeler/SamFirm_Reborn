36902d209826f1cf3c7921eaf9dec26c747116f0
hyeonho64 <hyeonhokang10@kakao.com>
2023-12-27 18:37:17 +0900

    Migrated to .net 8.0

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,15 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SamFirm Reborn")]
-[assembly: AssemblyFileVersion("0.3.6.8")]
-[assembly: AssemblyDescription("BornAgain Edition")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCopyright("Copyright zxz0O0 ©  2015 / Ivan Meler 2021-2023 / KSmith 2023 / corsicanu 2024")]
-[assembly: AssemblyTitle("SamFirm Reborn")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("f64041a3-90f5-4bee-a430-1c0f1d604eee")]
-[assembly: AssemblyVersion("1.0.0.1")]
